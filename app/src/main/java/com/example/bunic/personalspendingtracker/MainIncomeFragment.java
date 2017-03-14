0abf5bad57c9e99256fb90de1ffa110f949a2d0e
jurbunic <jbunic94@gmail.com>
2017-03-14 17:19:35 +0100

    Navigation for main income screen
    - Navigation layout file
    - Floating action menu for main income screen

--- app/src/main/java/com/example/bunic/personalspendingtracker/MainIncomeFragment.java
+++ app/src/main/java/com/example/bunic/personalspendingtracker/MainIncomeFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import butterknife.ButterKnife;
+import android.widget.LinearLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
@@ -15,13 +18,26 @@ import butterknife.ButterKnife;
 
 public class MainIncomeFragment extends Fragment {
 
-
+    @BindView(R.id.income_nav_menu)
+    LinearLayout income_menu;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_income_main, container, false);
         ButterKnife.bind(this,view);
         return view;
+    }
+
+
+    //----------------------Floating Action Menu Navigation-----------------------
+
+    @OnClick(R.id.income_nav_base_button)
+    public void onNavBaseClick(){
+        if(income_menu.getVisibility() == View.GONE){
+            income_menu.setVisibility(View.VISIBLE);
+        }else {
+            income_menu.setVisibility(View.GONE);
+        }
     }
 
 }
